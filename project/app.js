1a27d64d7cd2b283d7ce98cd0c8e8883f7f6d377
majothi9999 <110264390+majothi9999@users.noreply.github.com>
2022-11-25 20:00:25 -0800

    uplod

---
+++ project/app.js
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhMg-4XJrytgUqH2Uoojx8JZHzuOho2cA",
+  authDomain: "manjothi-f9052.firebaseapp.com",
+  projectId: "manjothi-f9052",
+  storageBucket: "manjothi-f9052.appspot.com",
+  messagingSenderId: "510248364489",
+  appId: "1:510248364489:web:af9657bbee7c9e46c9e018",
+  measurementId: "G-WTXY7N3Y3V"
+};
+ref,
+set,
+push,
+console.log(firebase)
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
+
+// let btnOpen = document.querySelector('button');
+// let input = document.querySelector('input');
+
+// buttonOpen.addEventListener('click', () =>{
+//     window.open(input.value,'black','height=300px,width=600px')
+// })
+
+
+//Flickity Gallery Slider
+// add the previous, selected, and next css classes
+Object.onclick.getElementById("submit");
+
+
+
+
